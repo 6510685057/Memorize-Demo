995677e7ecea6643977744030430285d8e3a0369
Yanatthan <6510685057@student.tu.ac.th>
2025-02-19 13:54:21 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by นางสาวญาณัชทัฬห์ คงกระจ่าง on 19/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MemorizeTests/MemorizeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeTests.swift
+//  MemorizeTests
+//
+//  Created by นางสาวญาณัชทัฬห์ คงกระจ่าง on 19/2/2568 BE.
+//
+
+import Testing
+@testable import Memorize
+
+struct MemorizeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
